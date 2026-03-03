579fe75ec91f119d1ab040030e93a7cd4b85042a
Lotharingia-BUR <135478406+Lotharingia-BUR@users.noreply.github.com>
2026-03-03 11:29:27 -0500

    create object in standard scene

---
+++ SawingDesignGame/Assets/StarterAssets/FirstPersonController/Scripts/ObjectPickup.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class ObjectPickup : MonoBehaviour
+{
+
+    public GameObject pickupObj;
+    public Transform pickupLocation;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
