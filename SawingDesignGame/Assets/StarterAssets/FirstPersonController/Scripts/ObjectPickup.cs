c3edd861d3add61237643809b7c6a32c447cb538
Lotharingia-BUR <135478406+Lotharingia-BUR@users.noreply.github.com>
2026-03-03 12:16:33 -0500

    kinda crashes the computer whoops

--- SawingDesignGame/Assets/StarterAssets/FirstPersonController/Scripts/ObjectPickup.cs
+++ SawingDesignGame/Assets/StarterAssets/FirstPersonController/Scripts/ObjectPickup.cs
@@ -5,15 +5,30 @@ public class ObjectPickup : MonoBehaviour
 
     public GameObject pickupObj;
     public Transform pickupLocation;
+    private Rigidbody pickupRigidbody;
+    private bool isHolding;
+    public float speed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        pickupRigidbody = pickupObj.GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (isHolding)
+        {
+            pickupObj.transform.position = Vector3.MoveTowards(pickupObj.transform.position, pickupLocation.position, speed * Time.deltaTime);
+            pickupObj.transform.position = Vector3.RotateTowards(pickupObj.transform.position, pickupLocation.position, speed * Time.deltaTime, 1);
+            /*pickupObj.transform.LookAt(pickupLocation.position);*/
+           /* pickupObj.transform.eulerAngles = pickupLocation.eulerAngles;*/
+        }
+        if (Input.GetKeyDown(KeyCode.E)) 
+        {
+            pickupObj.transform.parent = pickupLocation;
+            isHolding = true;
+            pickupRigidbody.useGravity = false;
+        }
     }
 }
