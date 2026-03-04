f5c3eeaeb656188765c29787e4c5b18b4b51bd66
Lotharingia-BUR <135478406+Lotharingia-BUR@users.noreply.github.com>
2026-03-04 13:00:51 -0500

    addingtherails

--- SawingDesignGame/Assets/New Scripts/MoveObjectScript.cs
+++ SawingDesignGame/Assets/New Scripts/MoveObjectScript.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 public class MoveObjectScript : MonoBehaviour
 {
     public GameObject laser;
+    public GameObject laserRailX;
+    public GameObject laserRailZ;
     public Vector3 laserDirection;
     public float relativeContraintX, relativeContraintZ;
+
     private float constraintU, constraintD, constraintL, constraintR;
     private Vector3 laserPos;
+    private Vector3 laserNewPos;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -22,9 +26,15 @@ public class MoveObjectScript : MonoBehaviour
     void Update()
     {
         laserPos = laser.transform.position;
-        if (laserPos.x > constraintD && laserPos.x < constraintU && laserPos.z < constraintL && laserPos.z > constraintR)
+        laserNewPos = laserPos + laserDirection * Time.deltaTime;
+
+        //Check to see if the new positon is valid
+        if (laserNewPos.x > constraintD 
+            && laserNewPos.x < constraintU
+            && laserNewPos.z < constraintL
+            && laserNewPos.z > constraintR)
         {
-            laser.transform.position += laserDirection * Time.deltaTime;
+            laser.transform.position = laserNewPos;
         }
     }
 }
