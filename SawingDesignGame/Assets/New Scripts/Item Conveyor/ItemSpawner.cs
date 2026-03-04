869c0ffa8edb896006364c7d783c3932c0037acd
Joshua-Bamford <bamforjo@sheridancollege.ca>
2026-03-04 14:21:09 -0500

    Completed Spawner
    Spawner instantiates object from array as well as playing the particle system. after 6 seconds the particles disappear

--- SawingDesignGame/Assets/New Scripts/Item Conveyor/ItemSpawner.cs
+++ SawingDesignGame/Assets/New Scripts/Item Conveyor/ItemSpawner.cs
@@ -7,13 +7,14 @@ public class ItemSpawner : MonoBehaviour
     GameObject currentObject;   //The object which will spawn next
     int chosenPositionInArray;
     float playTimer = 0;
+    bool objectSpawned;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     private Rigidbody objectRB;
     public ParticleSystem beamingParticles;
 
     void Start()
     {
-
+        objectSpawned = false;
     }
 
     // Update is called once per frame
@@ -25,38 +26,31 @@ public class ItemSpawner : MonoBehaviour
             {
                 chosenPositionInArray = Random.Range(0, earthObjects.Length);   //selects which index in the list of objects to choose
                 currentObject = earthObjects[chosenPositionInArray];    //selects the object that is in the chosen index as the one to spawn
-               // playTimer += playTimer + 1 * Time.deltaTime;
-               // Debug.Log(playTimer);
-               // if (playTimer > 3)
-               // {
-                    StartParticles();
                     AbductAnObject();
-
-               // }
+                objectSpawned = true;
             }
 
-
-            //if(currentObject != null && inCenter == false)  //when there is an object in the scene but it is not in the pickup area of the belt
-            //{
-            //    objectRB = currentObject.GetComponent<Rigidbody>(); //get the rigidbody from the currently present cuttable object
-            //    objectRB.AddForce(0, 0, conveyorSpeed, ForceMode.Force);    //applies the force in the direction of down the conveyor
-            //    Debug.Log("Force being applied");
-            //}
-
-            //if(currentObject.position)
         }
-    }
 
-    public void StartParticles()
-    {
-        beamingParticles.Play();
-    }
+        if (objectSpawned == true) {
+            playTimer += (playTimer + 0.1f) * Time.deltaTime;
+            Debug.Log(playTimer);
+        }
 
+        if (playTimer >= 6) {
+            beamingParticles.Pause();
+            beamingParticles.Clear();
+            objectSpawned = false;
+            playTimer = 0;
+        }
+
+    }
 
     public void AbductAnObject()
-    {
+    {   
+        beamingParticles.Play();
+        
         Instantiate(currentObject, transform.position, Quaternion.identity); //spawns the prefab at the position of the spawn point
-
     }
 
 
