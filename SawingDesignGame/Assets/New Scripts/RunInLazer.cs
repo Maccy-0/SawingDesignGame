5c1f82ef7102c55a3b86e2648dd878efcff62e89
Lotharingia-BUR <135478406+Lotharingia-BUR@users.noreply.github.com>
2026-03-05 22:49:48 -0500

    on enter

--- SawingDesignGame/Assets/New Scripts/RunInLazer.cs
+++ SawingDesignGame/Assets/New Scripts/RunInLazer.cs
@@ -1,5 +1,8 @@
+using System.Collections;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
+
 
 public class RunInLazer : MonoBehaviour
 {
@@ -7,13 +10,18 @@ public class RunInLazer : MonoBehaviour
     public GameObject Screen;
     public GameObject lazer;
     private Color fullAlpha;
+
+    private AudioSource soundSource;
+    public AudioClip soundEffect;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         dmgScreen = Screen.GetComponent<Image>();
         fullAlpha = dmgScreen.color;
-        fullAlpha.a = 100;
-        
+        fullAlpha.a = 0.9f;
+
+        soundSource = GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -26,7 +34,19 @@ public class RunInLazer : MonoBehaviour
         if (other.transform.name == lazer.transform.name)
         {
             dmgScreen.color = fullAlpha;
+            soundSource.PlayOneShot(soundEffect);
+            delaybytime(0.5f);
         }
         other = null;
+    }
+    
+    IEnumerator delaybytime(float maxDelay)
+    {
+        float delay = 0;
+        while (delay < maxDelay)
+        {
+            delay += Time.deltaTime;
+            yield return null;
+        }
     }
 }
