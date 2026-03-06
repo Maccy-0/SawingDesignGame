ceb9f9fa75071133a9aeeb58ec7922184b9b982c
Lotharingia-BUR <135478406+Lotharingia-BUR@users.noreply.github.com>
2026-03-05 21:08:36 -0500

    cursor and esscape updates

--- SawingDesignGame/Assets/New Scripts/BringMainMenu.cs
+++ SawingDesignGame/Assets/New Scripts/BringMainMenu.cs
@@ -1,17 +1,20 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class HideMouse : MonoBehaviour
+public class BringMainMenu : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        Cursor.visible = true;
-        Screen.SetResolution(1920, 1080, true);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene(0);
+        }
     }
 }
