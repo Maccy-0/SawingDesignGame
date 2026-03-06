72c4f72074c6e9aed39e1e21ce2097ed9447d0eb
Lotharingia-BUR <135478406+Lotharingia-BUR@users.noreply.github.com>
2026-03-05 23:02:33 -0500

    done

---
+++ SawingDesignGame/Assets/New Scripts/HideMouse1.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class HideMouse1 : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private void Start()
+    {
+        Screen.SetResolution(1920, 1080, true);
+    }
+}
