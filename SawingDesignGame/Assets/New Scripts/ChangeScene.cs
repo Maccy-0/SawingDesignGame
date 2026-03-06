79e6897ef65a2513a7eb87e889823be05df620d6
Lotharingia-BUR <135478406+Lotharingia-BUR@users.noreply.github.com>
2026-03-05 19:59:18 -0500

    sfx

--- SawingDesignGame/Assets/New Scripts/ChangeScene.cs
+++ SawingDesignGame/Assets/New Scripts/ChangeScene.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -6,15 +7,33 @@ public class ChangeScene : MonoBehaviour
 {
     public int SceneNumber;
     public Button yourself;
-
+    private AudioSource soundSource;
+    public AudioClip soundEffect;
 
     private void Start()
     {
+        soundSource = GetComponent<AudioSource>();
         yourself.onClick.AddListener(changeScene);
     }
 
     private void changeScene()
     {
+        soundSource.PlayOneShot(soundEffect);
+        StartCoroutine(delayedSceneChange(0.35f));
+        
+
+    }
+
+    IEnumerator delayedSceneChange(float maxDelay)
+    {
+        float delay = 0;
+        while (delay < maxDelay)
+        {
+            delay += Time.deltaTime;
+            Debug.Log(delay);
+            yield return null;
+        }
+        Debug.Log("done playing");
         SceneManager.LoadScene(SceneNumber);
     }
 }
